7f2c38337cbdb30cfce94c8d32cadb29e2cb8919
Mitch De Wilde <mitch.dewilde@ugent.be>
2015-12-01 21:40:09 +0100

    ActivitiesFragment - the beginning
    waiting on tabs to move functionality to correct
    (activities)Fragment/activity

--- app/src/main/java/be/ugent/zeus/hydra/fragments/HomeFragment.java
+++ app/src/main/java/be/ugent/zeus/hydra/fragments/HomeFragment.java
@@ -1,10 +1,27 @@
 package be.ugent.zeus.hydra.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import be.ugent.zeus.hydra.R;
 
 /**
  * Created by silox on 17/10/15.
  */
 
 public class HomeFragment extends Fragment {
-}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.homefragment_view, container, false);
+        EditText testtext = (EditText) view.findViewById(R.id.test);
+        testtext.setText("hello");
+        return view;
+    }}
