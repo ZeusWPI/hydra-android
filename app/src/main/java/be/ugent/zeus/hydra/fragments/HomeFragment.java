2ba58d8f6f736e0b588de434a53cbdf7c80c880b
Mitch De Wilde <mitch.dewilde@ugent.be>
2016-01-25 19:39:03 +0100

    started custom resto view

--- app/src/main/java/be/ugent/zeus/hydra/fragments/HomeFragment.java
+++ app/src/main/java/be/ugent/zeus/hydra/fragments/HomeFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import be.ugent.zeus.hydra.R;
+import be.ugent.zeus.hydra.models.RestoItem;
+import be.ugent.zeus.hydra.models.RestoMenu;
 
 /**
  * Created by silox on 17/10/15.
@@ -21,7 +23,23 @@ public class HomeFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.homefragment_view, container, false);
-        EditText testtext = (EditText) view.findViewById(R.id.test);
-        testtext.setText("hello");
+
+
+        // resto items for debug purposes
+        RestoItem[] meat = new RestoItem[3];
+        meat[0] = new RestoItem("spaghetti", "3.00", true);
+        meat[1] = new RestoItem("kaaskroket", "3.30", true);
+        meat[2] = new RestoItem("vegetarisch", "4.00", true);
+        String[] vegetables = new String[2];
+        vegetables[0] = "wortel";
+        vegetables[1] = "prei";
+        RestoMenu menu = new RestoMenu();
+        menu.setMeat(meat);
+        menu.setOpen(true);
+        menu.setVegetables(vegetables);
+
+
+
+
         return view;
     }}
--- app/src/main/java/be/ugent/zeus/hydra/models/RestoMenu.java
+++ app/src/main/java/be/ugent/zeus/hydra/models/RestoMenu.java
@@ -10,4 +10,45 @@ public class RestoMenu {
     private Date date;
     private RestoItem[] meat;
     private String[] vegetables;
+
+    public RestoMenu() {};
+
+    public RestoMenu(boolean open, Date date, RestoItem[] meat, String[] vegetables) {
+        this.open = open;
+        this.date = date;
+        this.meat = meat;
+        this.vegetables = vegetables;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public RestoItem[] getMeat() {
+        return meat;
+    }
+
+    public void setMeat(RestoItem[] meat) {
+        this.meat = meat;
+    }
+
+    public String[] getVegetables() {
+        return vegetables;
+    }
+
+    public void setVegetables(String[] vegetables) {
+        this.vegetables = vegetables;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
