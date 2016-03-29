a185f721339712c2b2c85d4bf70845aa0c53d336
Tom Naessens <Tom.Naessens@UGent.be>
2016-03-29 20:02:48 +0200

    Update libraries

---
+++ app/src/main/java/be/ugent/zeus/hydra/DrawerListItem.java
@@ -0,0 +1,7 @@
+package be.ugent.zeus.hydra;
+
+/**
+ * Created by silox on 19/10/15.
+ */
+public class DrawerListItem {
+}
---
+++ app/src/main/java/be/ugent/zeus/hydra/adapters/IconDrawerAdapter.java
@@ -0,0 +1,7 @@
+package be.ugent.zeus.hydra.adapters;
+
+/**
+ * Created by silox on 19/10/15.
+ */
+public class IconDrawerAdapter {
+}
