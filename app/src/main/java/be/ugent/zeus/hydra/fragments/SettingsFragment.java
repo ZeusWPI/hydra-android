d4b2d12ca26a874d6b9cf3c0042f121581027037
Ellen102 <eavhove@gmail.com>
2016-03-30 20:45:18 +0200

    selection xml

--- app/src/main/java/be/ugent/zeus/hydra/fragments/SettingsFragment.java
+++ app/src/main/java/be/ugent/zeus/hydra/fragments/SettingsFragment.java
@@ -16,6 +16,6 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.preferences);
+        addPreferencesFromResource(R.layout.preferences);
     }
 }
