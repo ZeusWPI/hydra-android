618f579058da28e7e00a335a743e023a95c88f3a
Ellen102 <eavhove@gmail.com>
2016-04-06 22:37:20 +0200

    only update on resume

--- app/src/main/java/be/ugent/zeus/hydra/fragments/ActivitiesFragment.java
+++ app/src/main/java/be/ugent/zeus/hydra/fragments/ActivitiesFragment.java
@@ -1,6 +1,5 @@
 package be.ugent.zeus.hydra.fragments;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,13 +17,13 @@ import be.ugent.zeus.hydra.R;
 import be.ugent.zeus.hydra.adapters.ActivityListAdapter;
 import be.ugent.zeus.hydra.models.association.AssociationActivities;
 import be.ugent.zeus.hydra.requests.AssociationActivitiesRequest;
-
 /**
  * Created by ellen on 2016-03-08.
+ *
+ * TODO: update after  settings changed.
  */
 
-public class ActivitiesFragment extends AbstractFragment implements
-        SharedPreferences.OnSharedPreferenceChangeListener {
+public class ActivitiesFragment extends AbstractFragment {
     private RecyclerView recyclerView;
     private ActivityListAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -64,7 +63,6 @@ public class ActivitiesFragment extends AbstractFragment implements
 
             @Override
             public void onRequestSuccess(final AssociationActivities associationActivitiesItems) {
-                System.out.println("------ updateeeee");
                 adapter.setItems(associationActivitiesItems.getPreferedActivities(getContext()));
                 adapter.notifyDataSetChanged();
             }
@@ -85,56 +83,17 @@ public class ActivitiesFragment extends AbstractFragment implements
                 .show();
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        System.out.println("------------- addaaaaapppttt");
-        performLoadActivityRequest();
-    }
 
-    /*
-    @Override
-public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
-        sharedPrefs.registerOnSharedPreferenceChangeListener(this);
-}
 
-    @Override
-    public void onStop() {
-        spiceManager.shouldStop();
-
-        super.onStop();
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
-        sharedPrefs.unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        // Set up a listener whenever a key changes
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
-
-        sharedPrefs
-                .registerOnSharedPreferenceChangeListener(this);
-    }
 
     @Override
     public void onPause() {
         super.onPause();
-        // Unregister the listener whenever a key changes
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
-sharedPrefs
-        .unregisterOnSharedPreferenceChangeListener(this);
+        performLoadActivityRequest();
+
     }
 
-    protected SpiceManager spiceManager = new SpiceManager(GsonSpringAndroidSpiceService.class);
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        spiceManager.start(getContext());
-    }
-*/
 
 
 }
