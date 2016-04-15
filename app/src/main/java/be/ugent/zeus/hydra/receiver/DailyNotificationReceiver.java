0774ac0a90cdfed6cc69e3a1dc6dfe7a9740d8d1
Rien Maertens <maertensrien@gmail.com>
2016-04-15 23:27:17 +0200

    Only vibrate when the user wants it.

--- app/src/main/java/be/ugent/zeus/hydra/receiver/DailyNotificationReceiver.java
+++ app/src/main/java/be/ugent/zeus/hydra/receiver/DailyNotificationReceiver.java
@@ -8,6 +8,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 
 import be.ugent.zeus.hydra.R;
@@ -26,17 +27,21 @@ public class DailyNotificationReceiver extends BroadcastReceiver {
         // TODO: Open resto
         PendingIntent pIntent = PendingIntent.getActivity(context, 0, tmpIntent, 0);
 
-        Notification n  = new Notification.Builder(context)
+        Notification.Builder builder = new Notification.Builder(context)
                 .setContentTitle("Resto menu")
                 .setContentText("Dit is een test.")
                 .setSmallIcon(R.drawable.logo)
-                .setVibrate(new long[] {0, 500})
                 .setContentIntent(pIntent)
-                .setAutoCancel(true).build();
+                .setAutoCancel(true);
+
+        if (PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean("pref_key_daily_notifications_vibrate", false)){
+            builder.setVibrate(new long[] {0, 500});
+        }
 
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0, n);
+        notificationManager.notify(0, builder.build());
     }
 }
