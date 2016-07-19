5c4e4b030a2f4fe4865f7e59421368c603ce30d7
Niko Strijbol <strijbol.niko@gmail.com>
2016-07-20 00:08:27 +0200

    Clean up

--- app/src/main/java/be/ugent/zeus/hydra/utils/html/Utils.java
+++ app/src/main/java/be/ugent/zeus/hydra/utils/html/Utils.java
@@ -10,24 +10,6 @@ import android.text.Spanned;
 public class Utils {
 
     /**
-     * Strip HTML.
-     *
-     * TODO this code is not very performant
-     *
-     * @param html The HTML to strip.
-     *
-     * @return Stripped HTML.
-     */
-    public static CharSequence stripHtml(String html) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            //noinspection deprecation
-            return Html.fromHtml(html).toString();
-        } else {
-            return Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT).toString();
-        }
-    }
-
-    /**
      * Helper with older version support.
      *
      * @param html The HTML to strip.
