121b1ef6ebc2f7a1ce3475e2c338292c08a68be7
Niko Strijbol <strijbol.niko@gmail.com>
2016-07-25 23:43:02 +0200

    More vectors + remove unused images

--- app/src/main/java/be/ugent/zeus/hydra/utils/ViewUtils.java
+++ app/src/main/java/be/ugent/zeus/hydra/utils/ViewUtils.java
@@ -1,8 +1,15 @@
 package be.ugent.zeus.hydra.utils;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.TypedValue;
 import android.view.View;
 import be.ugent.zeus.hydra.R;
@@ -32,5 +39,27 @@ public class ViewUtils {
         TypedValue typedValue = new TypedValue();
         activity.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
         return typedValue.data;
+    }
+
+    /**
+     * Get a drawable (also works for vectors) in the given color.
+     *
+     * @param context A context
+     * @param drawable The drawable to get
+     * @param color The color to tint the drawable in
+     * @return The drawable
+     */
+    public static Drawable getTintedDrawable(Context context, @DrawableRes int drawable, @ColorRes int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Drawable d = context.getDrawable(drawable);
+            assert d != null;
+            d.setTint(context.getColor(color));
+            return d;
+        } else {
+            Drawable d = ContextCompat.getDrawable(context, drawable);
+            Drawable dw = DrawableCompat.wrap(d);
+            DrawableCompat.setTint(dw, ContextCompat.getColor(context, color));
+            return dw;
+        }
     }
 }
