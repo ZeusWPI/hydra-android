b939d78b295b88785be10cb1ae715c2a75a441a8
Niko Strijbol <strijbol.niko@gmail.com>
2016-09-11 14:48:09 +0200

    Fix display of dates

--- app/src/main/java/be/ugent/zeus/hydra/models/sko/Artist.java
+++ app/src/main/java/be/ugent/zeus/hydra/models/sko/Artist.java
@@ -2,10 +2,12 @@ package be.ugent.zeus.hydra.models.sko;
 
 import android.support.annotation.Nullable;
 
-import be.ugent.zeus.hydra.models.converters.DateTimeAdapter;
+import be.ugent.zeus.hydra.models.converters.ZonedThreeTenAdapter;
 import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
-import org.joda.time.DateTime;
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.ZoneId;
+import org.threeten.bp.ZonedDateTime;
 
 import java.io.Serializable;
 
@@ -14,28 +16,65 @@ import java.io.Serializable;
  *
  * @author Niko Strijbol
  */
+@SuppressWarnings("unused")
 public class Artist implements Serializable {
 
     @SerializedName("artist")
     private String name;
-    @JsonAdapter(DateTimeAdapter.class)
-    private DateTime start;
-    @JsonAdapter(DateTimeAdapter.class)
-    private DateTime end;
+    @JsonAdapter(ZonedThreeTenAdapter.class)
+    private ZonedDateTime start;
+    @JsonAdapter(ZonedThreeTenAdapter.class)
+    private ZonedDateTime end;
     private String picture;
 
+    /**
+     * @return The name of the act.
+     */
     public String getName() {
         return name;
     }
 
-    public DateTime getStart() {
+    /**
+     * @return The start date, with time zone information.
+     */
+    public ZonedDateTime getStart() {
         return start;
     }
 
-    public DateTime getEnd() {
+    /**
+     * @return The end date, with time zone information.
+     */
+    public ZonedDateTime getEnd() {
         return end;
     }
 
+    /**
+     * Get the start date, converted to the local time zone. The resulting DateTime is the time as it is used
+     * in the current time zone.
+     *
+     * This value is calculated every time, so if you need it a lot, cache it in a local variable.
+     *
+     * @return The converted start date.
+     */
+    public LocalDateTime getLocalStart() {
+        return getStart().withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    /**
+     * Get the end date, converted to the local time zone. The resulting DateTime is the time as it is used
+     * in the current time zone.
+     *
+     * This value is calculated every time, so if you need it a lot, cache it in a local variable.
+     *
+     * @return The converted end date.
+     */
+    public LocalDateTime getLocalEnd() {
+        return getEnd().withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    /**
+     * @return The URL to the picture.
+     */
     @Nullable
     public String getPicture() {
         return picture;
--- app/src/main/java/be/ugent/zeus/hydra/recyclerview/viewholder/sko/LineupViewHolder.java
+++ app/src/main/java/be/ugent/zeus/hydra/recyclerview/viewholder/sko/LineupViewHolder.java
@@ -7,8 +7,11 @@ import android.widget.TextView;
 import be.ugent.zeus.hydra.R;
 import be.ugent.zeus.hydra.models.sko.Artist;
 import be.ugent.zeus.hydra.recyclerview.viewholder.DataViewHolder;
-import be.ugent.zeus.hydra.utils.DateUtils;
 import com.squareup.picasso.Picasso;
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.Locale;
 
 import static be.ugent.zeus.hydra.utils.ViewUtils.$;
 
@@ -16,6 +19,9 @@ import static be.ugent.zeus.hydra.utils.ViewUtils.$;
  * @author Niko Strijbol
  */
 public class LineupViewHolder extends DataViewHolder<Artist> {
+
+    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM - HH:mm", new Locale("nl"));
+    private static final DateTimeFormatter hourFormat = DateTimeFormatter.ofPattern("HH:mm", new Locale("nl"));
 
     private TextView title;
     private TextView date;
@@ -31,8 +37,35 @@ public class LineupViewHolder extends DataViewHolder<Artist> {
 
     public void populate(final Artist artist) {
         title.setText(artist.getName());
-        date.setText(DateUtils.getFriendlyDate(artist.getStart().toDate()));
-
+        date.setText(getDisplayDate(artist));
         Picasso.with(this.itemView.getContext()).load(artist.getPicture()).into(image);
+    }
+
+    /**
+     * Get the display date. The resulting string is of the following format:
+     *
+     * dd/MM HH:mm tot [dd/MM] HH:mm
+     *
+     * The second date is only shown of it is not the same date as the first.
+     *
+     * @param artist The artist.
+     *
+     * @return The text to display.
+     */
+    private String getDisplayDate(Artist artist) {
+
+        LocalDateTime start = artist.getLocalStart();
+        LocalDateTime end = artist.getLocalEnd();
+
+        //Add the start date and time
+        String date = start.format(format) + " tot ";
+
+        if(start.getDayOfMonth() == end.getDayOfMonth()) {
+            date += end.format(hourFormat);
+        } else {
+            date +=  end.format(format);
+        }
+
+        return date;
     }
 }
