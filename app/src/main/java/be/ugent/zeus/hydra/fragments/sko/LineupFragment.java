0387dad0487e81b42c89cbaedd156169166790bd
Niko Strijbol <strijbol.niko@gmail.com>
2016-09-11 15:50:01 +0200

    Add swipe to refresh

--- app/src/main/java/be/ugent/zeus/hydra/fragments/sko/LineupFragment.java
+++ app/src/main/java/be/ugent/zeus/hydra/fragments/sko/LineupFragment.java
@@ -3,17 +3,14 @@ package be.ugent.zeus.hydra.fragments.sko;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 
 import be.ugent.zeus.hydra.R;
-import be.ugent.zeus.hydra.cache.CachedAsyncTaskLoader;
-import be.ugent.zeus.hydra.fragments.common.LoaderFragment;
-import be.ugent.zeus.hydra.loader.ThrowableEither;
+import be.ugent.zeus.hydra.cache.CacheRequest;
+import be.ugent.zeus.hydra.fragments.common.CachedLoaderFragment;
 import be.ugent.zeus.hydra.models.sko.Stage;
 import be.ugent.zeus.hydra.models.sko.Stages;
 import be.ugent.zeus.hydra.recyclerview.adapters.sko.LineupAdapter;
@@ -22,17 +19,26 @@ import be.ugent.zeus.hydra.requests.sko.LineupRequest;
 import static be.ugent.zeus.hydra.utils.ViewUtils.$;
 
 /**
+ * Show the lineup.
+ *
  * @author Niko Strijbol
  */
-public class LineupFragment extends LoaderFragment<Stages> {
+public class LineupFragment extends CachedLoaderFragment<Stages> implements SwipeRefreshLayout.OnRefreshListener {
 
-    protected RecyclerView recyclerView;
-    protected LineupAdapter adapter;
+    private RecyclerView recyclerView;
+    private LineupAdapter adapter;
+    private SwipeRefreshLayout refreshLayout;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recycler_view, container, false);
+        return inflater.inflate(R.layout.fragment_sko_lineup, container, false);
     }
 
     @Override
@@ -40,18 +46,15 @@ public class LineupFragment extends LoaderFragment<Stages> {
         super.onViewCreated(view, savedInstanceState);
 
         recyclerView = $(view, R.id.recycler_view);
+        refreshLayout = $(view, R.id.refresh_layout);
+
+        refreshLayout.setOnRefreshListener(this);
 
         adapter = new LineupAdapter();
-
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
-    /**
-     * This must be called when data is received that has no errors.
-     *
-     * @param data The data.
-     */
     @Override
     public void receiveData(@NonNull Stages data) {
 
@@ -59,10 +62,35 @@ public class LineupFragment extends LoaderFragment<Stages> {
         Stage stage = data.get(0);
 
         adapter.setItems(stage.getArtists());
+        
+        refreshLayout.setRefreshing(false);
     }
 
     @Override
-    public Loader<ThrowableEither<Stages>> onCreateLoader(int id, Bundle args) {
-        return new CachedAsyncTaskLoader<>(new LineupRequest(), getContext());
+    public void onRefresh() {
+        shouldRenew = true;
+        restartLoader();
+        shouldRenew = false;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_sko_refresh, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_refresh) {
+            onRefresh();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public CacheRequest<Stages, Stages> getRequest() {
+        return new LineupRequest();
     }
 }
