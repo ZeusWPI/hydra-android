1631736d43593599af701980098beb1e1b55ab7c
Niko Strijbol <strijbol.niko@gmail.com>
2016-09-11 21:50:36 +0200

    Long press line-up to add to calendar

--- app/src/main/java/be/ugent/zeus/hydra/recyclerview/viewholder/sko/LineupViewHolder.java
+++ app/src/main/java/be/ugent/zeus/hydra/recyclerview/viewholder/sko/LineupViewHolder.java
@@ -1,5 +1,10 @@
 package be.ugent.zeus.hydra.recyclerview.viewholder.sko;
 
+import android.content.Intent;
+import android.provider.CalendarContract;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,14 +23,18 @@ import static be.ugent.zeus.hydra.utils.ViewUtils.$;
 /**
  * @author Niko Strijbol
  */
-public class LineupViewHolder extends DataViewHolder<Artist> {
+public class LineupViewHolder extends DataViewHolder<Artist> implements View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
 
     private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM - HH:mm", new Locale("nl"));
     private static final DateTimeFormatter hourFormat = DateTimeFormatter.ofPattern("HH:mm", new Locale("nl"));
 
+    private static final int MENU_ID_ADD_TO_CALENDAR = 0;
+    private static final String LOCATION = "Sint-Pietersplein, Gent";
+
     private TextView title;
     private TextView date;
     private ImageView image;
+    private Artist artist;
 
     public LineupViewHolder(View itemView) {
         super(itemView);
@@ -33,9 +42,12 @@ public class LineupViewHolder extends DataViewHolder<Artist> {
         title = $(itemView, R.id.title);
         date = $(itemView, R.id.date);
         image = $(itemView, R.id.card_image);
+
+        itemView.setOnCreateContextMenuListener(this);
     }
 
     public void populate(final Artist artist) {
+        this.artist = artist;
         title.setText(artist.getName());
         date.setText(getDisplayDate(artist));
         Picasso.with(this.itemView.getContext()).load(artist.getPicture()).into(image);
@@ -67,5 +79,31 @@ public class LineupViewHolder extends DataViewHolder<Artist> {
         }
 
         return date;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if(artist != null) {
+            menu.add(Menu.NONE, MENU_ID_ADD_TO_CALENDAR, 0, "Toevoegen aan agenda").setOnMenuItemClickListener(this);
+        }
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+
+        if(artist != null && item.getItemId() == MENU_ID_ADD_TO_CALENDAR) {
+            Intent intent = new Intent(Intent.ACTION_INSERT)
+                    .setData(CalendarContract.Events.CONTENT_URI)
+                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, artist.getStart().toInstant().toEpochMilli())
+                    .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, artist.getEnd().toInstant().toEpochMilli())
+                    .putExtra(CalendarContract.Events.TITLE, artist.getName())
+                    .putExtra(CalendarContract.Events.EVENT_LOCATION, LOCATION)
+                    .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
+
+            title.getContext().startActivity(intent);
+            return true;
+        }
+
+        return false;
     }
 }
