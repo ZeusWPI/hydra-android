61e8b735eb68a20bcad88d051a9b6d93e14ddeb5
Niko Strijbol <strijbol.niko@gmail.com>
2016-09-12 19:56:40 +0200

    Compatibility fix + improve showing of SKO card

--- app/src/main/java/be/ugent/zeus/hydra/requests/SpecialRemoteEventRequest.java
+++ app/src/main/java/be/ugent/zeus/hydra/requests/SpecialRemoteEventRequest.java
@@ -4,8 +4,10 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 
 import be.ugent.zeus.hydra.BuildConfig;
+import be.ugent.zeus.hydra.R;
 import be.ugent.zeus.hydra.models.specialevent.SpecialEvent;
 import be.ugent.zeus.hydra.models.specialevent.SpecialEventWrapper;
+import be.ugent.zeus.hydra.models.specialevent.SpecialEvents;
 import be.ugent.zeus.hydra.requests.common.Request;
 import be.ugent.zeus.hydra.requests.common.RequestFailureException;
 import com.google.android.gms.tasks.Tasks;
@@ -25,7 +27,6 @@ public class SpecialRemoteEventRequest implements Request<SpecialEventWrapper> {
     public static final String REMOTE_SKO_KEY = "is_sko_card_enabled";
 
     private FirebaseRemoteConfig config;
-
     private SpecialEventRequest request;
 
     public SpecialRemoteEventRequest() {
@@ -34,33 +35,48 @@ public class SpecialRemoteEventRequest implements Request<SpecialEventWrapper> {
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
                 .setDeveloperModeEnabled(BuildConfig.DEBUG)
                 .build();
+        config.setDefaults(R.xml.remote_config);
         config.setConfigSettings(configSettings);
     }
 
     @NonNull
     @Override
     public SpecialEventWrapper performRequest() throws RequestFailureException {
-        SpecialEventWrapper wrapper = request.performRequest();
 
-        //Add the SKO card if necessary
+        //Empty wrapper.
+        SpecialEventWrapper wrapper = new SpecialEventWrapper();
+        wrapper.setSpecialEvents(new SpecialEvents());
+
+        //Add the SKO card if necessary.
         try {
             Tasks.await(config.fetch(86400)); //Blocking fetching
             config.activateFetched();
 
-            if(config.getBoolean(REMOTE_SKO_KEY)) {
-                Log.d(TAG, "Adding SKO card.");
-                SpecialEvent event = new SpecialEvent();
-                event.setName("Student Kick-Off");
-                event.setSimpleText("Ga naar de info voor de Student Kick-Off");
-                event.setImage("http://www.studentkickoff.be/sites/all/themes/SKO2013/assets/images/logo-shadow.png");
-                event.setSko(true);
-                event.setPriority(1010);
-                wrapper.getSpecialEvents().add(event);
-            } else {
-                Log.d(TAG, "Not adding SKO card.");
-            }
         } catch (ExecutionException | InterruptedException e) {
             Log.w(TAG, "Error while getting remote config.", e);
+        }
+
+        if(config.getBoolean(REMOTE_SKO_KEY)) {
+            Log.d(TAG, "Adding SKO card.");
+            SpecialEvent event = new SpecialEvent();
+            event.setName("Student Kick-Off");
+            event.setSimpleText("Ga naar de info voor de Student Kick-Off");
+            event.setImage("http://www.studentkickoff.be/sites/all/themes/SKO2013/assets/images/logo-shadow.png");
+            event.setSko(true);
+            event.setPriority(1010);
+            wrapper.getSpecialEvents().add(event);
+        } else {
+            Log.d(TAG, "Not adding SKO card.");
+        }
+
+        //Add normal special events.
+        try {
+            wrapper.getSpecialEvents().addAll(request.performRequest().getSpecialEvents());
+        } catch (RequestFailureException e) {
+            //If there are no special events, we pass along the error.
+            if(wrapper.getSpecialEvents().isEmpty()) {
+                throw e;
+            }
         }
 
         return wrapper;
