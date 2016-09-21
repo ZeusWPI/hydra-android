ee7720b23c671665c373a4a7bd9daa4897f178ab
Niko Strijbol <strijbol.niko@gmail.com>
2016-09-21 17:50:46 +0200

    Small fixes

--- app/src/main/java/be/ugent/zeus/hydra/recyclerview/viewholder/sko/ExhibitorViewHolder.java
+++ app/src/main/java/be/ugent/zeus/hydra/recyclerview/viewholder/sko/ExhibitorViewHolder.java
@@ -1,5 +1,7 @@
 package be.ugent.zeus.hydra.recyclerview.viewholder.sko;
 
+import android.support.v7.app.AlertDialog;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,6 +10,7 @@ import be.ugent.zeus.hydra.R;
 import be.ugent.zeus.hydra.models.sko.Exhibitor;
 import be.ugent.zeus.hydra.recyclerview.viewholder.DataViewHolder;
 import be.ugent.zeus.hydra.utils.ViewUtils;
+import be.ugent.zeus.hydra.utils.html.Utils;
 import com.squareup.picasso.Picasso;
 
 import static be.ugent.zeus.hydra.utils.ViewUtils.$;
@@ -29,10 +32,23 @@ public class ExhibitorViewHolder extends DataViewHolder<Exhibitor> {
     }
 
     @Override
-    public void populate(Exhibitor data) {
+    public void populate(final Exhibitor data) {
         name.setText(data.getName());
         int size = ViewUtils.convertDpToPixelInt(80, itemView.getContext());
         Picasso.with(itemView.getContext()).load(data.getLogo()).resize(size, size).onlyScaleDown().centerInside().into(imageView);
-        content.setText(data.getContent());
+
+        final Spanned converted = Utils.fromHtml(data.getContent());
+
+        content.setText(converted);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle(data.getName());
+                builder.setMessage(converted);
+                builder.setPositiveButton("OK", null);
+                builder.show();
+            }
+        });
     }
 }
