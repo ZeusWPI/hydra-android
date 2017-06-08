e33c996939b6be6f071a99307fb251a95c2c4ef4
Niko Strijbol <strijbol.niko@gmail.com>
2017-06-08 14:08:31 +0200

    Add more information to refresh broadcast

--- app/src/main/java/be/ugent/zeus/hydra/repository/RefreshBroadcast.java
+++ app/src/main/java/be/ugent/zeus/hydra/repository/RefreshBroadcast.java
@@ -12,11 +12,15 @@ public class RefreshBroadcast {
 
     public static final String BROADCAST = "be.ugent.zeus.hydra.data.refresh";
     public static final String REFRESH_COLD = "be.ugent.zeus.hydra.data.refresh.cold";
+    public static final String REFRESH_TYPE = "be.ugent.zeus.hydra.data.refresh.type";
+
+    public static final int REFRESH_ALL = -1;
 
     public static void broadcastRefresh(Context context, boolean cold) {
         LocalBroadcastManager manager = LocalBroadcastManager.getInstance(context);
         Intent intent = new Intent(BROADCAST);
         intent.putExtra(REFRESH_COLD, cold);
+        intent.putExtra(REFRESH_TYPE, REFRESH_ALL);
         manager.sendBroadcast(intent);
     }
 
