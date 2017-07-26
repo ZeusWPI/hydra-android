9a151ca0ed5a8dd91510adcb49ae6ec9671a1d2f
Niko Strijbol <strijbol.niko@gmail.com>
2017-07-26 23:29:15 +0200

    Add support for multiple base adapters

--- app/src/main/java/be/ugent/zeus/hydra/ui/common/recyclerview/EmptyViewObserver.java
+++ app/src/main/java/be/ugent/zeus/hydra/ui/common/recyclerview/EmptyViewObserver.java
@@ -3,13 +3,15 @@ package be.ugent.zeus.hydra.ui.common.recyclerview;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import java8.util.stream.StreamSupport;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Class that manages displaying and hiding the RecyclerView and the empty View.
  *
  * If the adapter has no data, the View will displayed. If there is data, the RecyclerView will be displayed.
- *
- * The RecyclerView must have access to the adapter before data events begin. This means you must have called
- * {@link RecyclerView#setAdapter(RecyclerView.Adapter)} before calling {@link RecyclerView.Adapter#registerAdapterDataObserver(RecyclerView.AdapterDataObserver)}.
  *
  * @author Niko Strijbol
  */
@@ -17,14 +19,30 @@ public class EmptyViewObserver extends RecyclerView.AdapterDataObserver {
 
     private final RecyclerView recyclerView;
     private final View emptyView;
+    private final AdapterConsolidator consolidator;
 
     /**
+     * When using this constructor, you must register the adapter with the RecyclerView before calling this.
+     *
      * @param recyclerView The RecyclerView. Will be shown when there is data.
      * @param emptyView The View to display when there is no data.
      */
     public EmptyViewObserver(RecyclerView recyclerView, View emptyView) {
+        this(recyclerView, emptyView, new AdapterConsolidator(recyclerView.getAdapter()));
+    }
+
+    /**
+     * The RecyclerView must have access to the adapter before data events begin. This means you must have called
+     * {@link RecyclerView#setAdapter(RecyclerView.Adapter)} before calling
+     * {@link RecyclerView.Adapter#registerAdapterDataObserver(RecyclerView.AdapterDataObserver)}.
+     *
+     * @param recyclerView The RecyclerView. Will be shown when there is data.
+     * @param emptyView The View to display when there is no data.
+     */
+    public EmptyViewObserver(RecyclerView recyclerView, View emptyView, AdapterConsolidator consolidator) {
         this.recyclerView = recyclerView;
         this.emptyView = emptyView;
+        this.consolidator = consolidator;
     }
 
     @Override
@@ -57,19 +75,31 @@ public class EmptyViewObserver extends RecyclerView.AdapterDataObserver {
         showHide();
     }
 
-    private boolean isEmpty() {
-        if (recyclerView.getAdapter() == null) {
-            throw new IllegalStateException("You must register the Adapter before registering this class.");
+    private void showHide() {
+        if (consolidator.isEmpty()) {
+            recyclerView.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
         } else {
-            return recyclerView.getAdapter().getItemCount() == 0;
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
         }
     }
 
-    private void showHide() {
-        if (isEmpty()) {
-            emptyView.setVisibility(View.VISIBLE);
-        } else {
-            emptyView.setVisibility(View.GONE);
+    public static class AdapterConsolidator {
+
+        private Collection<RecyclerView.Adapter<?>> adapters = new ArrayList<>();
+
+        public AdapterConsolidator(RecyclerView.Adapter<?> baseAdapter) {
+            adapters.add(baseAdapter);
+        }
+
+        boolean isEmpty() {
+            return StreamSupport.stream(adapters).allMatch(a -> a.getItemCount() == 0);
+        }
+
+        public AdapterConsolidator add(RecyclerView.Adapter<?> adapter) {
+            adapters.add(adapter);
+            return this;
         }
     }
 }
