7f5f22ca0b99c8bafe47599d5769228a6a67cbb5
Niko Strijbol <strijbol.niko@gmail.com>
2017-12-14 02:12:44 +0100

    Seemingly a lot of UI changes: - Move to new UGent colours - Change all text to the default text colours, since the new blue is tad too flashy. - Implement the Material Design standard better in regards to lists.

--- app/src/main/java/be/ugent/zeus/hydra/ui/main/info/InfoViewHolder.java
+++ app/src/main/java/be/ugent/zeus/hydra/ui/main/info/InfoViewHolder.java
@@ -2,6 +2,8 @@ package be.ugent.zeus.hydra.ui.main.info;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 import be.ugent.zeus.hydra.R;
@@ -25,18 +27,21 @@ class InfoViewHolder extends DataViewHolder<InfoItem> {
 
     @Override
     public void populate(final InfoItem infoItem) {
-
+        Context c = itemView.getContext();
         title.setText(infoItem.getTitle());
         itemView.setOnClickListener(v -> infoItem.getType().doOnClick(v.getContext(), infoItem));
 
-        int color = R.color.ugent_blue_dark;
-        Context c = itemView.getContext();
-        Drawable more = infoItem.getType().getDrawable(c, color);
+        // Get the primary colour of the app.
+        TypedValue typedValue = new TypedValue();
+        c.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+
+        int color = ContextCompat.getColor(c, typedValue.resourceId);
+        Drawable more = infoItem.getType().getDrawable(itemView.getContext(), typedValue.resourceId);
 
         //If the item itself has an image.
         if (infoItem.getImage() != null) {
             int resId = c.getResources().getIdentifier(infoItem.getImage(), "drawable", c.getPackageName());
-            Drawable icon = ViewUtils.getTintedVectorDrawable(c, resId, color);
+            Drawable icon = ViewUtils.getTintedVectorDrawable(c, resId, typedValue.resourceId);
             title.setCompoundDrawablesWithIntrinsicBounds(icon, null, more, null);
         } else {
             title.setCompoundDrawablesWithIntrinsicBounds(null, null, more, null);
