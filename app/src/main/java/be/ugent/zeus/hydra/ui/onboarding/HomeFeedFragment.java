06a738d919cced7bb04b601f6843d451ebd628be
Niko Strijbol <strijbol.niko@gmail.com>
2018-01-04 16:50:12 +0100

    Add support for Account Transfer API

--- app/src/main/java/be/ugent/zeus/hydra/ui/onboarding/HomeFeedFragment.java
+++ app/src/main/java/be/ugent/zeus/hydra/ui/onboarding/HomeFeedFragment.java
@@ -1,6 +1,7 @@
 package be.ugent.zeus.hydra.ui.onboarding;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
@@ -18,12 +19,12 @@ public class HomeFeedFragment extends SlideFragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_onboarding_home, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         FragmentManager manager = getChildFragmentManager();
         manager.beginTransaction().add(R.id.recycler_fragment, new HomeFeedSelectFragment()).commit();
