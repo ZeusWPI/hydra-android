165c378b8b8fcda37257fda9e5e08a61ed2d6a64
Niko Strijbol <strijbol.niko@gmail.com>
2018-02-09 00:00:57 +0100

    Ignore test (see #248)

--- app/src/test/java/be/ugent/zeus/hydra/common/database/migrations/TestMigration_13_14.java
+++ app/src/test/java/be/ugent/zeus/hydra/common/database/migrations/TestMigration_13_14.java
@@ -8,6 +8,7 @@ import android.support.annotation.RequiresApi;
 import be.ugent.zeus.hydra.BuildConfig;
 import be.ugent.zeus.hydra.TestApp;
 import be.ugent.zeus.hydra.common.database.Database;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,6 +40,7 @@ public class TestMigration_13_14 {
     }
 
     @Test
+    @Ignore
     public void testMigration() throws IOException {
         // There is no data to test, since we just add a new column.
         SupportSQLiteDatabase version12 = testHelper.createDatabase("test-db", 13);
