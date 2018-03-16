27dc121e20ac5781b2f1369062ba7a2cef64f3fa
Niko Strijbol <strijbol.niko@gmail.com>
2018-03-16 18:00:33 +0100

    Migrate event request to new network stack

--- app/src/main/java/be/ugent/zeus/hydra/association/event/RawEventRequest.java
+++ app/src/main/java/be/ugent/zeus/hydra/association/event/RawEventRequest.java
@@ -3,13 +3,11 @@ package be.ugent.zeus.hydra.association.event;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import be.ugent.zeus.hydra.common.caching.Cache;
 import be.ugent.zeus.hydra.common.network.Endpoints;
-import be.ugent.zeus.hydra.common.network.JsonSpringRequest;
-import be.ugent.zeus.hydra.common.request.CacheableRequest;
+import be.ugent.zeus.hydra.common.network.JsonOkHttpRequest;
 import be.ugent.zeus.hydra.common.request.Request;
-import be.ugent.zeus.hydra.common.request.Requests;
 import java8.util.function.Function;
+import org.threeten.bp.Duration;
 
 import java.util.List;
 
@@ -21,18 +19,12 @@ import java.util.List;
  * @author feliciaan
  * @author Niko Strijbol
  */
-public class RawEventRequest extends JsonSpringRequest<Event[]> implements CacheableRequest<Event[]> {
+public class RawEventRequest extends JsonOkHttpRequest<List<Event>> {
 
     private static final String FILENAME = "all_activities.json";
 
-    @SuppressWarnings("WeakerAccess")
-    public RawEventRequest() {
-        super(Event[].class);
-    }
-
-    @NonNull
-    public String getCacheKey() {
-        return FILENAME;
+    RawEventRequest(Context context) {
+        super(context, listToken(Event.class));
     }
 
     @NonNull
@@ -42,13 +34,12 @@ public class RawEventRequest extends JsonSpringRequest<Event[]> implements Cache
     }
 
     @Override
-    public long getCacheDuration() {
-        return Cache.ONE_HOUR;
+    public Duration getCacheDuration() {
+        return Duration.ofHours(1);
     }
 
     /**
      * Transform the {@link RawEventRequest} by applying:
-     * - {@link Requests#cachedList(Context, CacheableRequest)}
      * - {@link Request#map(Function)} with {@link DisabledEventRemover}
      * - {@link Request#map(Function)} with {@link EventSorter}
      *
@@ -57,7 +48,7 @@ public class RawEventRequest extends JsonSpringRequest<Event[]> implements Cache
      * @return The modified request.
      */
     public static Request<List<Event>> cachedFilteredSortedRequest(Context context) {
-        return Requests.cachedList(context, new RawEventRequest())
+        return new RawEventRequest(context)
                 .map(new DisabledEventRemover(context))
                 .map(new EventSorter());
     }
