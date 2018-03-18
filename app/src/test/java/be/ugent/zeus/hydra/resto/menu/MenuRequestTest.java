f87a3f1f0b2d048a286162f2ca8d42e0001b3041
Niko Strijbol <strijbol.niko@gmail.com>
2018-03-18 21:30:09 +0100

    Convert menu test

--- app/src/test/java/be/ugent/zeus/hydra/resto/menu/MenuRequestTest.java
+++ app/src/test/java/be/ugent/zeus/hydra/resto/menu/MenuRequestTest.java
@@ -1,14 +1,10 @@
 package be.ugent.zeus.hydra.resto.menu;
 
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.preference.PreferenceManager;
-import android.support.annotation.RequiresApi;
 
-import be.ugent.zeus.hydra.BuildConfig;
-import be.ugent.zeus.hydra.TestApp;
-import be.ugent.zeus.hydra.common.network.ArrayJsonSpringRequestTest;
-import be.ugent.zeus.hydra.common.network.JsonSpringRequest;
+import be.ugent.zeus.hydra.common.network.AbstractJsonRequestTest;
+import be.ugent.zeus.hydra.common.network.JsonOkHttpRequest;
 import be.ugent.zeus.hydra.resto.RestoMenu;
 import be.ugent.zeus.hydra.resto.RestoPreferenceFragment;
 import org.junit.Assert;
@@ -16,22 +12,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.annotation.Config;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Niko Strijbol
  */
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, application = TestApp.class)
-@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-public class MenuRequestTest extends ArrayJsonSpringRequestTest<RestoMenu> {
+public class MenuRequestTest extends AbstractJsonRequestTest<List<RestoMenu>> {
 
-    private static final String[] restos = new String[] {
-            "nl-debrug",
+    private static final String[] RESTAURANTS = new String[] {
+            "nl-debrug", // This is the default one.
             "nl",
             "en",
             "nl-heymans",
@@ -39,18 +31,14 @@ public class MenuRequestTest extends ArrayJsonSpringRequestTest<RestoMenu> {
             "nl-kantienberg"
     };
 
-    public MenuRequestTest() {
-        super(RestoMenu[].class);
+    @Override
+    protected String getRelativePath() {
+        return "resto/menu_default.json";
     }
 
     @Override
-    protected Resource getSuccessResponse() {
-        return new ClassPathResource("resto/menu_default.json");
-    }
-
-    @Override
-    protected JsonSpringRequest<RestoMenu[]> getRequest() {
-        return null;
+    protected JsonOkHttpRequest<List<RestoMenu>> getRequest() {
+        return new MenuRequest(RuntimeEnvironment.application);
     }
 
     @Test
@@ -63,13 +51,13 @@ public class MenuRequestTest extends ArrayJsonSpringRequestTest<RestoMenu> {
     @Test
     public void testOtherUrl() {
         // Get random other resto, not the default one.
-        final int RANDOM_RESTO = ThreadLocalRandom.current().nextInt(1, restos.length);
+        final int random_resto = ThreadLocalRandom.current().nextInt(1, RESTAURANTS.length);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(RuntimeEnvironment.application);
         preferences.edit()
-                .putString(RestoPreferenceFragment.PREF_RESTO_KEY, restos[RANDOM_RESTO])
+                .putString(RestoPreferenceFragment.PREF_RESTO_KEY, RESTAURANTS[random_resto])
                 .apply();
 
         MenuRequest request = new MenuRequest(RuntimeEnvironment.application);
-        Assert.assertEquals(String.format(MenuRequest.OVERVIEW_URL, restos[RANDOM_RESTO]), request.getAPIUrl());
+        Assert.assertEquals(String.format(MenuRequest.OVERVIEW_URL, RESTAURANTS[random_resto]), request.getAPIUrl());
     }
 }
