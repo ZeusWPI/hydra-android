14381f8d9f6a0d31e55cf532b9db0acee436a403
Niko Strijbol <strijbol.niko@gmail.com>
2018-06-14 23:50:04 +0200

    Fix test

--- app/src/test/java/be/ugent/zeus/hydra/utils/DateUtilsTest.java
+++ app/src/test/java/be/ugent/zeus/hydra/utils/DateUtilsTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 import org.threeten.bp.DayOfWeek;
 import org.threeten.bp.LocalDate;
 import org.threeten.bp.format.DateTimeFormatter;
@@ -22,6 +23,7 @@ import static org.junit.Assert.assertEquals;
  * @author Niko Strijbol
  */
 @RunWith(RobolectricTestRunner.class)
+@Config(qualifiers = "nl")
 public class DateUtilsTest {
 
     @Test
