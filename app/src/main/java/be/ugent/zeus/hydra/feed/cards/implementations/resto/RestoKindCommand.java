345171c1063c0810cd3113cb9810aac42c046eb2
Niko Strijbol <strijbol.niko@gmail.com>
2019-01-07 00:31:13 +0100

    Fix for #289

--- app/src/main/java/be/ugent/zeus/hydra/feed/cards/implementations/resto/RestoKindCommand.java
+++ app/src/main/java/be/ugent/zeus/hydra/feed/cards/implementations/resto/RestoKindCommand.java
@@ -1,7 +1,6 @@
 package be.ugent.zeus.hydra.feed.cards.implementations.resto;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.util.SparseArray;
 import android.util.SparseIntArray;
 
@@ -19,13 +18,33 @@ import static be.ugent.zeus.hydra.feed.cards.implementations.resto.RestoCardView
  */
 class RestoKindCommand implements FeedCommand {
 
+    /**
+     * Map the menu items to their resulting view state, i.e. what should be shown after the menu item is pressed.
+     *
+     * For example, the {@link RestoCardViewHolder.KindMenu#HIDE_SOUP}
+     * menu item id is mapped to {@link HomeFragment.FeedRestoKind#MAIN}, meaning when the hide soup menu is pressed,
+     * the resulting menu type is one that only shows the main menu.
+     *
+     * It is important to note that one of the two must always be shown, e.g. soup and/or main dishes, but not none.
+     */
     private static final SparseArray<String> MENU_TO_FORWARD = new SparseArray<>();
+    /**
+     * The reverse mapping of {@link #MENU_TO_FORWARD}. This maps the menu items to their view state when the menu item
+     * was pressed, effectively reversing the action.
+     */
     private static final SparseArray<String> MENU_TO_REVERSE = new SparseArray<>();
+    /**
+     * Maps menu items to their message of completion, i.e. the message shown when the action has been performed.
+     */
     private static final SparseIntArray MENU_TO_ITEM = new SparseIntArray();
     static {
+        // When hiding soup, only main dishes remain.
         MENU_TO_FORWARD.append(HIDE_SOUP, HomeFragment.FeedRestoKind.MAIN);
+        // When showing soup, all is shown.
         MENU_TO_FORWARD.append(SHOW_SOUP, HomeFragment.FeedRestoKind.ALL);
+        // When hiding main dishes, only soup remains.
         MENU_TO_FORWARD.append(HIDE_MAIN, HomeFragment.FeedRestoKind.SOUP);
+        // When showing main dishes, all is shown.
         MENU_TO_FORWARD.append(SHOW_MAIN, HomeFragment.FeedRestoKind.ALL);
 
         MENU_TO_REVERSE.append(HIDE_SOUP, MENU_TO_FORWARD.get(SHOW_SOUP));
@@ -34,9 +53,9 @@ class RestoKindCommand implements FeedCommand {
         MENU_TO_REVERSE.append(SHOW_MAIN, MENU_TO_FORWARD.get(HIDE_MAIN));
 
         MENU_TO_ITEM.append(HIDE_SOUP, R.string.feed_card_hidden_menu_soup);
-        MENU_TO_ITEM.append(SHOW_SOUP, R.string.feed_card_hidden_menu_soup);
+        MENU_TO_ITEM.append(SHOW_SOUP, R.string.feed_card_shown_menu_soup);
         MENU_TO_ITEM.append(HIDE_MAIN, R.string.feed_card_hidden_menu_main);
-        MENU_TO_ITEM.append(SHOW_MAIN, R.string.feed_card_hidden_menu_main);
+        MENU_TO_ITEM.append(SHOW_MAIN, R.string.feed_card_shown_menu_main);
     }
 
     @RestoCardViewHolder.KindMenu
