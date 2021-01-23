da97d4b1ee17a9cddf4d2486a1cb9640c9c7cbf1
Niko Strijbol <strijbol.niko@gmail.com>
2021-01-23 18:13:07 +0100

    Re-enable migration tests
    Robolectric has updated the SQLite version (Robolectric#5216), so this
    should work again.

--- app/src/test/java/be/ugent/zeus/hydra/common/database/migrations/TestMigration_9_10.java
+++ app/src/test/java/be/ugent/zeus/hydra/common/database/migrations/TestMigration_9_10.java
@@ -28,7 +28,6 @@ import static org.mockito.Mockito.when;
  *
  * @author Niko Strijbol
  */
-@Ignore("Enable once Robolectric updates SQL")
 @RunWith(RobolectricTestRunner.class)
 public class TestMigration_9_10 {
 
