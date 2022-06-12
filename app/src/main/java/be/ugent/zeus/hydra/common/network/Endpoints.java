07366b7a3827c5c574e2a66983b222d943f3806f
Tom Naessens <siloxtom@gmail.com>
2022-06-12 18:25:52 +0200

    Use new Tab API endpoint

--- app/src/main/java/be/ugent/zeus/hydra/common/network/Endpoints.java
+++ app/src/main/java/be/ugent/zeus/hydra/common/network/Endpoints.java
@@ -34,7 +34,7 @@ public interface Endpoints {
     String ZEUS_V2 = "https://hydra.ugent.be/api/2.0/";
 
     String TAP = "https://tap.zeus.gent/";
-    String TAB = "https://tab.zeus.gent/";
+    String TAB = "https://tab.zeus.gent/api/v1/";
     String MATTERMORE = "https://mattermore.zeus.gent/";
 
     String LIBRARY = "https://widgets.lib.ugent.be/";
