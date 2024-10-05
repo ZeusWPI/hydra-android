684f26e2c03123439cee87d9f8c3168392064b09
Niko Strijbol <strijbol.niko@gmail.com>
2024-10-05 18:09:56 +0200

    Migrate settings.gradle to Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2021 The Hydra authors
+ * Copyright (c) 2015-2024 The Hydra authors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,5 +20,5 @@
  * SOFTWARE.
  */
 
-include ':app'
-include ':material-intro'
+include(":app")
+include(":material-intro")
