a816410b8ddd8a1d8b6220b5ef3c679640014ed0
Niko Strijbol <strijbol.niko@gmail.com>
2024-10-05 22:13:05 +0200

    Use upstream snapshots

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,6 +33,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
         maven { url = uri("https://jitpack.io") }
     }
 }
